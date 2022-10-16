eb9190e5150f092117257548837c171b63e05f7f
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-16 15:40:07 -0700

    .

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char wol;
+
+	for (wol = 'z'; wol <= 'a'; wol++)
+	{
+		putchar(wol);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
