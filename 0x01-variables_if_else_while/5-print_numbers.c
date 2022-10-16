878a7a795463205712dd118cab0d2f84291fa52f
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-16 15:09:18 -0700

    .

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int babe;
+
+	for (babe = 0; babe <=9; babe++)
+	{
+		printf("%d", babe);
+	}
+	printf("\n");
+
+	return (0);
+
+}
