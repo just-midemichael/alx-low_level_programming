24ac0b2f901dad4cd2d0e48520e9a049dfba2e76
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-16 15:22:38 -0700

    .

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int g;
+
+	for (g = '0' ; g <= '9'; g++)
+	{
+		putchar(g);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
