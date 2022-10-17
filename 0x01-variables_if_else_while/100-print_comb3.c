90df129fc3c77f72c1ce8c8fbca8dba3e01c7f99
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-16 18:05:31 -0700

    .

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -22,8 +22,10 @@ int main(void)
 				putchar(d);
 				putchar(p);
 
-				if (d == 'g' && p == '9')
+				if (d == '8' && p == '9')
+				{
 					continue;
+				}
 
 				putchar(',');
 				putchar(' ');
