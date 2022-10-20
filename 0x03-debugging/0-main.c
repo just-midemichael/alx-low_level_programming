4819f85571a09f4e18f693b344fd4712c1ef880d
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-20 10:34:14 -0700

    .

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or nagative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+
+	i = 0;
+	positive_or_nagative(i);
+
+	return (0);
+
+}
