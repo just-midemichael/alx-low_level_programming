d319843795dcd770fb436fede15bd0910cb8d2ce
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-20 10:59:59 -0700

    .

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an ifinite loop
+ * main - causes an infinite loop
  *
  * Return: 0
  */
@@ -15,10 +15,10 @@ int main(void)
 
 	i = 0;
 
-	/*	while (i < 10)	*/
-	/*	{	*/
-	/*		putchar(i);	*/
-	/*	}	*/
+	/*while (i < 10)	*/
+	/*{	*/
+	/*	putchar(i);	*/
+	/*}	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
