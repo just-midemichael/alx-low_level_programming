bcfaf98386733555c69022a906a18b67059bb1dc
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-21 00:16:05 -0700

    .

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -4,7 +4,7 @@
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
- * @c: thirds integer
+ * @c: third integer
  * Retun: largest number
  */
 
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
