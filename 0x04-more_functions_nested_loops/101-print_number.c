7a9863cb0a9c7bc14d7db45b8cf8e49b915e0684
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-22 13:08:25 -0700

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,18 @@ void print_integer(int m);
 void print_number(int n)
 {
 	if (n == 0)
+	{
 		_putchar('0');
+	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		print_integer(n * -1);
 	}
 	else
+	{
 		print_integer(n);
+	}
 }
 
 /**
@@ -31,8 +35,10 @@ void print_integer(int m)
 	int i = 1000000000;
 
 	for (; i >= 1; i /= 10)
+	{
 		if (m / i != 0)
 		{
 			_putchar((m / i) % 10 + '0');
 		}
+	}
 }
