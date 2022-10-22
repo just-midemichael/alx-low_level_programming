466d6b6687d9af9ab9689e40c51a801c6f580957
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-22 07:52:24 -0700

    .

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,5 +8,16 @@
 
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+	return (isupper);
 }
