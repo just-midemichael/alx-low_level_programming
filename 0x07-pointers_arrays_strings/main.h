7455b2f1f3eeed3ea3c671af26b3a0e4485b3386
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-10-31 09:37:55 -0700

    .

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Description: Header file containing prototypes for all functions
+ * used in the 0x07-pointers_arrays_strings directory
+ */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
