68b34c70e56435e8c60fd17e639c8c03c4430ff8
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-11-02 14:27:06 -0700

    .

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,14 +9,17 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
 	{
 		return (-1);
 	}
-
-	if (y > 0)
+	else if (y == 1)
 	{
-		return ( x * _pow_recursion(x, y - 1));
+		return (x);
 	}
-	return (1);
+	return (x *= _pow_recursion(x, y - 1));
 }
