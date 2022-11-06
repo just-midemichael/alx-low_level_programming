dcab4e65a7f633ad4fe3d36060254fe8185a082b
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-11-06 13:29:20 -0800

    .

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+
+		printf("%d\n", result);
+		return (0);
+	}
+}
