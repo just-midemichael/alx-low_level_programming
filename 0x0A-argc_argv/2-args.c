d6ba9010aa246927265c2e5e45e12e2646f608f6
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-11-06 12:38:48 -0800

    .

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * program that prints all arguments it receives
+ * including the first one and ending with a new line
+ * @argc - counts the arguments
+ * @argv - arrays of the argunments value
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
