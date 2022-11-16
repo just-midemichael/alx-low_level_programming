7d1061797cb675734617ccff4c366a17288c38b9
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-11-15 20:08:29 -0800

    .

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,16 +35,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	cpyname = malloc(len_name + 1);
 	if (cpyname == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; name[i]; i++)
+	{
 		cpyname[i] = name[i];
+	}
 	cpyname[i] = '\0';
 
 	cpyowner = malloc(len_owner + 1);
 	if (cpyowner == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; owner[i]; i++)
+	{
 		cpyowner[i] = owner[i];
+	}
 	cpyowner[i] = '\0';
 
 	new_dog->name = cpyname;
