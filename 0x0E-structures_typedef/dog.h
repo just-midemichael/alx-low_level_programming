e1f087a00e9888e4f8ab472eaf157cb323927b46
Olumide Michael Ojo <olumide.m.ojo@gmail.com>
2022-11-15 19:09:26 -0800

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,6 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- *
  * Description: Longer description
  */
 
@@ -22,7 +21,6 @@ struct dog
  */
 
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
